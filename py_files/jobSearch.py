714624bb3b2829367bf54b6c23c0462963735a13
e2oo <e2.tejeda@gmail.com>
2016-08-28 14:22:31 -0400

    Uploaded right py file

--- py_files/jobSearch.py
+++ py_files/jobSearch.py
@@ -1,29 +1,64 @@
-# Import bs4
+import webbrowser
+import os
+
 from bs4 import BeautifulSoup
-import requests
+from selenium import webdriver
 
-# Get the html form requested page
-html = requests.get('http://www.indeed.com/')
-#
-htmlText = html.text
-indeed_soup = BeautifulSoup(htmlText, "html.parser")
+# job_input = input('Enter the job title: ')
+job_input = 'software engineer'
+job_input = job_input.replace(" ", "+")
+
+# loc_input = input('Enter the location (City,State): ')
+loc_input = 'bridgeport,ct'
+loc_input = loc_input.replace(",","%2C+")
+
+# New url with user inputs
+searchUrl = 'http://www.indeed.com/jobs?q=' + job_input + '&l=' + loc_input
+
+# Goto webpage using headless browser
+browser = webdriver.PhantomJS()
+browser.get(searchUrl)
+
+# let's parse our html
+soup = BeautifulSoup(browser.page_source, "html.parser")
+
+# Finds the results list
+#find_job_titles = soup.find_all(attrs={'class': 'row result'})
+
+i = 0
+# Find job title/company name/location/ href
+#for row in soup.find_all(attrs={"class": "result"}):
+find_job_titles = soup.find_all(attrs={'data-tn-element': 'jobTitle'})
+company_list = {}
+job_title = []
+company_name = []
+for row in find_job_titles:
+    div_ = row.find_previous('div')
+    company_ = div_.select('.company')
+    job_title = row.attrs['title']
+
+    for comp in company_:
+
+        company_name = comp.text.lstrip().replace('\n', '')
+        #company_list.insert(i, company_name)
+
+        company_list[job_title] = company_name
+        print(job_title, " - ", company_name)
+
+    i = i + 1
 
 
-# tag.string.replace_with('Hello there')
-# print(indeed_soup.title)
-indeed_div = indeed_soup.div
-indeed_div_contents = indeed_div.contents[5]
+# Create html
+path = os.path.abspath('jobSearch.html')
+message = """<html><head></head><body><p>Indeed Results</p></body></html>"""
 
+url = 'file://' + path
 
-indeed_div_contents_list = indeed_div_contents.find_all('div')
-# html_div = indeed_soup.find_all('div')
-# div_content = html_div.contents
-# print(indeed_div)
+find_job_span = soup.select('span.company')
 
-print(indeed_div_contents_list[6].contents)
-#print(type(soup))
+with open(path,'w') as f:
 
-# print(soup.prettify()[10000:15000])
-# print('-----------------')
+    f.write(message)
+#f.close()
 
-
+webbrowser.open(url)
